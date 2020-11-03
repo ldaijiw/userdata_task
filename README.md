06dd3722d5be06a57d92868ee13f834582f54004
ldaijiw <lwaltmann@spartaglobal>
2020-11-03 10:29:23 +0000

    readme and task completed

---
+++ README.md
@@ -0,0 +1,17 @@
+# User data task
+
+You can assign user input to variables using input(), with a question in the form of a string to ask the user for their data. e.g
+
+```
+name = input("What's your name?)
+```
+
+Repeat for each piece of data:
+- first name
+- middle name
+- last name
+- address
+- postcode
+- NI number
+- Course applied
+- Most recent education
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # userdata_task
+
+'''
+using input() to take trainee data from users
+using casting and string methods to format them correctly
+'''
+
+first_name = input("What is your first name? ").lower().capitalize()
+
+middle_name = input("What is your middle name? ").lower().capitalize()
+
+last_name = input("What is your last name? ").lower().capitalize()
+
+address = input("What is the first line of your address? ").lower().capitalize()
+
+postcode = input("Please enter your postcode: ").upper()
+
+ni_number = input("Please enter your NI number: ").upper()
+
+course_applied = input("What course did you apply for? ").lower().capitalize()
+
+most_recent_edu = input("What is your most recent education? ").lower().capitalize()
+
+print(f"Hello {first_name} {middle_name} {last_name}. Your details have been saved.")
