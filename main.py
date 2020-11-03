05a658876e20068e2e6a01b1fb8e6d150b6bc2e6
ldaijiw <lwaltmann@spartaglobal>
2020-11-03 10:50:48 +0000

    changed capitalize to title where applicable

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ middle_name = input("What is your middle name? ").lower().capitalize()
 
 last_name = input("What is your last name? ").lower().capitalize()
 
-address = input("What is the first line of your address? ").lower().capitalize()
+address = input("What is the first line of your address? ").lower().title()
 
 postcode = input("Please enter your postcode: ").upper()
 
 ni_number = input("Please enter your NI number: ").upper()
 
-course_applied = input("What course did you apply for? ").lower().capitalize()
+course_applied = input("What course did you apply for? ").lower().title()
 
-most_recent_edu = input("What is your most recent education? ").lower().capitalize()
+most_recent_edu = input("What is your most recent education? ").lower().title()
 
 print(f"Hello {first_name} {middle_name} {last_name}. Your details have been saved.")
 
